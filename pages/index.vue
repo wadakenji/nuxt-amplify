ef52647621d14a34183b191f6509f7e75039e9d0
oka hidenobu <okahidenobu@gmail.com>
2020-05-12 23:03:38 +0900

    no message

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>Github repositories</h3>
+    <pre>{{ viewer.repositories }}</pre>
+  </div>
+</template>
+
+<script>
+import getRepositories from "~/apollo/queries/getRepositories.graphql";
+
+export default {
+  data() {
+    return {
+      viewer: {}
+    };
+  },
+
+  apollo: {
+    viewer: {
+      query: getRepositories
+    }
+  }
+};
+</script>
