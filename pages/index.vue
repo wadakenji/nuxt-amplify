c9c4739233881becb8940f9e9ae219867ea4ed59
和田健嗣 <ponporopen@gmail.com>
2020-06-06 16:33:05 +0900

    aboutページ追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,25 @@
 <template>
   <div>
+    <nuxt-link to="/about">To about page</nuxt-link>
     <h3>Github repositories</h3>
     <pre>{{ viewer.repositories }}</pre>
   </div>
 </template>
 
 <script>
-import getRepositories from "~/apollo/queries/getRepositories.graphql";
+  import getRepositories from "~/apollo/queries/getRepositories.graphql"
 
-export default {
-  data() {
-    return {
-      viewer: {}
-    };
-  },
+  export default {
+    data() {
+      return {
+        viewer: {}
+      }
+    },
 
-  apollo: {
-    viewer: {
-      query: getRepositories
+    apollo: {
+      viewer: {
+        query: getRepositories
+      }
     }
   }
-};
 </script>
