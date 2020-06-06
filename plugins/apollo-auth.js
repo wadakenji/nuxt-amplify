f63da62a525dabf2fca4e2ec4c4b9c604b1770f1
和田健嗣 <ponporopen@gmail.com>
2020-06-06 15:33:30 +0900

    環境変数名変更

--- plugins/apollo-auth.js
+++ plugins/apollo-auth.js
@@ -1,9 +1,9 @@
-export default function(context) {
+export default function (context) {
   //08e327ea24407bdb0925a4585cd6251a058d233d
-  const accessToken = process.env.accessToken;
+  const githubAccessToken = process.env.GITHUB_ACCESS_TOKEN
 
   return {
     httpEndpoint: "https://api.github.com/graphql",
-    getAuth: () => `Bearer ${accessToken}`
-  };
+    getAuth: () => `Bearer ${githubAccessToken}`
+  }
 }
